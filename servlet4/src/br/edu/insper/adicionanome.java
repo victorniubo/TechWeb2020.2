c8f238cb6223acc675d4c50be728c8acec80ec32
Luís Filipe Loureiro <luisfml@al.insper.edu.br>
2020-09-07 16:54:38 -0300

    Método get feito

--- servlet4/src/br/edu/insper/adicionanome.java
+++ servlet4/src/br/edu/insper/adicionanome.java
@@ -34,6 +34,21 @@ public class adicionanome extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String curso = request.getParameter("curso");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<h2>");
+        out.println("Nome: " + nome );
+        out.println("</h2>");
+        out.println("<h2>");
+        out.println("Email: " + email );
+        out.println("</h2>");
+        out.println("<h2>");
+        out.println("Curso: " + curso );
+        out.println("</h2>");
+        out.println("</html>");
 		
 		
 
